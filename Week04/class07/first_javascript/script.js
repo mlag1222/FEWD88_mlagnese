a0a9437a20076460d0c1961bc14118e2926dd58d
mlag1222 <lagnese.matt@gmail.com>
2016-07-01 10:04:05 -0400

    naming conventions, finished relaxer

---
+++ Week04/class07/first_javascript/script.js
@@ -0,0 +1,34 @@
+alert('hi');
+
+	document.write('<h2>writing to the document, like a boss</h2>');
+
+	console.log("hello");
+
+	var fname = "Matt";
+	var lname = "Lagnese";
+	var num1 = 10 * 10;
+
+	document.write(( 10 * 10 ) + '</br>');
+	document.write(fname + " " + lname + '</br>');
+	document.write(num1 + '</br>');
+	document.write(num1 - 100 + '</br>');
+	document.write(num1 * 100 + '</br');
+
+
+	// function definition
+
+	function doSomething () {
+		alert('this is an alert. this is annoying');
+	}
+
+
+	document.getElementById('greeting').style.color = "red";
+
+	var myPara = document.getElementById('myPara');
+	
+	myPara.style.color = "green";
+	myPara.onclick = doSomething;
+
+
+
+
