a033b8185894c6b67c078c0f2e016916231a2e01
mlag1222 <lagnese.matt@gmail.com>
2016-07-27 21:05:52 -0400

    wk 8 classwork and homework

--- Week08/Homework/startup-matchmaker-responsive/js/untitled.js
+++ Week08/Homework/startup-matchmaker-responsive/js/untitled.js
@@ -2,11 +2,10 @@ $(document).ready(function () {
 	// console.logs the window.width
 	$(window).resize(function () {
 		console.log($(window).width())
-		if ($(window).width() >= 1024 ) {
-			$('#mobile-nav').hide();
-		}
 	})
-	$('#burger').click(function(){
-		$('#mobile-nav').slideToggle();
+
+	// toggle mobile nav
+	$('#burger').click(function () {
+		$('#mobnav').slideToggle();
 	});
 })
