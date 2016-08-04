be66c77a223a753906a07c31e180c1ca7f72f532
mlag1222 <lagnese.matt@gmail.com>
2016-08-03 20:56:39 -0400

    ch ch ch changes

---
+++ Week09/Final/app.js
@@ -0,0 +1,17 @@
+$("#heat").click(function(){
+	$(".quote").html('Brother, you are going down');
+});
+
+$("#godfather").click(function(){
+	$(".quote").html('I know it was you. You broke my heart.');
+});
+	
+
+$("#scarface").click(function(){
+	$(".quote").html('Say hello to my leetle fren!');
+});
+
+$("#scent").click(function(){
+	$(".quote").html('If I was the man I was five years ago, Id take a FLAMETHROWER TO THIS PLACE!');
+});
+	
